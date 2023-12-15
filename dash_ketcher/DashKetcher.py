912643c91fa8c7053152ac8cc26ce043e37df191
yaosk <yaosk@dp.tech>
2023-12-15 20:06:29 +0800

    update

---
+++ dash_ketcher/DashKetcher.py
@@ -0,0 +1,42 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class DashKetcher(Component):
+    """A DashKetcher component.
+ExampleComponent is an example component.
+It takes a property, `label`, and
+displays it.
+It renders an input with the property `value`
+which is editable by the user.
+
+Keyword arguments:
+
+- id (string; optional)
+
+- buttonLabel (string; optional)
+
+- button_className (string; optional)
+
+- button_style (dict; optional)
+
+- molecule (string; optional)
+
+- style (dict; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_ketcher'
+    _type = 'DashKetcher'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, molecule=Component.UNDEFINED, style=Component.UNDEFINED, buttonLabel=Component.UNDEFINED, button_className=Component.UNDEFINED, button_style=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'buttonLabel', 'button_className', 'button_style', 'molecule', 'style']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'buttonLabel', 'button_className', 'button_style', 'molecule', 'style']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        super(DashKetcher, self).__init__(**args)
